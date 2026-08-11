03875a6e1b67c649954e2b226b3e7e6c73bafb98
Robert Williams III <225978092+SolSabaliEnt@users.noreply.github.com>
2026-08-11 17:34:46 -0500

    Reserve About route

---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+import { FoundationPlaceholder } from "@/components/ui/FoundationPlaceholder";
+
+export default function AboutPage() {
+  return <FoundationPlaceholder label="About Us" title="About Us" />;
+}

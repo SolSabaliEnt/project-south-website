fb3d289f004c0f947105754fb488886020f813f3
Robert Williams III <225978092+SolSabaliEnt@users.noreply.github.com>
2026-08-11 17:35:18 -0500

    Reserve press release archive route

---
+++ src/app/press-releases/page.tsx
@@ -0,0 +1,11 @@
+import { FoundationPlaceholder } from "@/components/ui/FoundationPlaceholder";
+
+export default function PressReleasesPage() {
+  return (
+    <FoundationPlaceholder
+      label="Press Releases"
+      title="Press Releases"
+      note="This route will become the searchable, filterable press release archive. Individual releases will not appear in the global navigation."
+    />
+  );
+}
